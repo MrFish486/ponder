bd6e0d0200678c071a24088dd49d5f10020b9009
MrFish486 <laszlodeanpav@gmail.com>
2025-08-19 17:43:35 -0700

    There seems to be a problem. Debugging now.

--- checks.js
+++ checks.js
@@ -2,4 +2,4 @@ const imports = require("./imports.js");
 
 let cube = new imports.primitives.box.box([-10,-10,-10], [10,10,10]);
 
-console.log(cube.overlaps([0,0,0]));
+console.log(cube.overlaps([-9,-9,-9]));
--- scene.js
+++ scene.js
@@ -3,11 +3,13 @@ const imports = require("./imports.js");
 var output = new imports.image.image(100, 100);
 var shader = new imports.shader.shader(1);
 var cube = new imports.primitives.box.box([0, 0, 0], [10, 10, 10], shader);
-var cam = new imports.camerabox.camerabox([-10,0,0], [-9,10,10], output);
+var cam = new imports.camerabox.camerabox([-10,0,0], [-9,1,1], output);
 var bounding_box = new imports.primitives.box.box([-100, -100, -100], [100, 100, 100]);
 var env = new imports.environment.environment(cam, [cube], [], bounding_box);
 var light = new imports.primitives.light_source.light_source([15, 15, 15], 10000, env);
 
+console.log(env.photons);
+
 env.render(1000000);
 
-console.log(output);
+console.log(JSON.stringify(output));
